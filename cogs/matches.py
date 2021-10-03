406929dfc0c85ece883ceafe785b22db11a863e9
tristan <heytristaann@gmail.com>
2021-10-03 08:24:39 -0400

    c

--- cogs/matches.py
+++ cogs/matches.py
@@ -11,11 +11,26 @@ class Matches(commands.Cog):
 
 
     @commands.command()
-    async def match(self, ctx, action:str, match_id):
+    async def match(self, ctx, action:str, match_id:int):
         if action == "report":
-            pass
+            for row in cur.execute(f"SELECT * FROM matches WHERE guild_id = {ctx.guild.id} AND match_id = {match_id}"):
+                cur.execute(f"UPDATE matches SET status = 'reported' WHERE guild_id = {ctx.guild.id} AND match_id = {match_id}")
+                db.commit()
+
         if action == "cancel":
-            pass
+            for row in cur.execute(f"SELECT * FROM matches WHERE guild_id = {ctx.guild.id} AND match_id = {match_id}"):
+                cur.execute(f"UPDATE matches SET status = 'cancelled' WHERE guild_id = {ctx.guild.id} AND match_id = {match_id}")
+                db.commit()
+            
+                embed=discord.Embed(title=f"Match #{match_id} | CANCELLED", description=f"**Map:** {row[2]}", color=65535)
+                embed.add_field(name="Orange Captain", value=f"<@{row[3]}>")
+                embed.add_field(name="\u200b", value="\u200b")
+                embed.add_field(name="Blue Captain", value=f"<@{row[5]}>")
+                embed.add_field(name="Orange Team", value='\n'.join(f"<@{e}>" for e in str(row[4]).split(",")))
+                embed.add_field(name="\u200b", value="\u200b")
+                embed.add_field(name="Blue Team", value='\n'.join(f"<@{e}>" for e in str(row[6]).split(",")))
+                await ctx.send(embed=embed)
+
         if action == "show":
             for row in cur.execute(f'SELECT * FROM matches WHERE guild_id = {ctx.guild.id} AND match_id = {match_id}'):
                 embed=discord.Embed(title=f"Match #{match_id} | {row[7].upper()}", description=f"**Map:** {row[2]}", color=65535)
@@ -44,6 +59,13 @@ class Matches(commands.Cog):
             embed.add_field(name="Blue Team", value='\n'.join(f"<@{e}>" for e in str(row[6]).split(",")))
             await ctx.send(embed=embed)
 
+
+
+    @commands.command(aliases=["sub"])
+    async def replace(self, ctx, match_id:int, user1:discord.Member, user2:discord.Member):
+        pass
+
+        
 
 
 
