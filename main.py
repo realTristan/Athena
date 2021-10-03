3d29942e95e1b671e999418c1cd1cd73675254e7
tristan <heytristaann@gmail.com>
2021-10-03 12:50:35 -0400

    c

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ cur.execute(f'''CREATE TABLE IF NOT EXISTS matches (guild_id int, match_id int, 
 db.commit()
 
 @client.event
+async def on_member_leave(ctx, member):
+    if cur.execute(f"SELECT EXISTS(SELECT 1 FROM users WHERE guild_id = {ctx.guild.id} AND user_id = {member.id});").fetchall()[0] == (1,):
+        cur.execute(f"DELETE FROM users WHERE guild_id = {ctx.guild.id} AND user_id = {member.id};")
+        db.commit()
+
+@client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         return
