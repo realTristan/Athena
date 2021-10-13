a519370f42d25863805005673ffb59ee5a52b0e8
tristan <heytristaann@gmail.com>
2021-10-12 23:04:41 -0400

    c

--- main.py
+++ main.py
@@ -38,22 +38,6 @@ async def on_ready():
     print(f'Launched: {client.user.name} // {client.user.id}')
     await client.change_presence(activity=discord.Game(name="Commands | =help"))
 
-@client.command(description="Loads an extention")
-async def load(ctx, extention):
-    client.load_extension(f'cogs.{extention}')
-    await ctx.send(f"**Loaded {extention}**", delete_after=2)
-
-@client.command(description="Unloads an extention")
-async def unload(ctx, extention):
-    client.unload_extension(f'cogs.{extention}')
-    await ctx.send(f"**Unloaded {extention}**", delete_after=2)
-
-@client.command(description="Reloads an extention")
-async def reload(ctx, extention):
-    client.unload_extension(f'cogs.{extention}')
-    client.load_extension(f'cogs.{extention}')
-    await ctx.send(f"**Reloaded {extention}**", delete_after=2)
-
 for filename in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cogs')):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
