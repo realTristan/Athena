40bacedb9e0a5791c5734d8df5d185d4931fd01e
tristan <heytristaann@gmail.com>
2021-10-16 08:21:01 -0400

    c

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ for filename in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__
         print(f'Loaded: cog.{filename[:-3]}')
 
 
-client.run('ODgzMDA2NjA5MjgwODY0MjU3.YTDp_Q.FMx7JjkGJShTA_7_u1VCmIlz1qM')
+client.run('YOUR BOT TOKEN')
