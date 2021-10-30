6c6e2b34331123c71f4c36dfc089138702b7d11d
tristan <heytristaann@gmail.com>
2021-10-30 07:00:41 -0400

    c

--- _cache/_cache_.py
+++ _cache/_cache_.py
@@ -1,19 +1,16 @@
 from _sql import *
 import asyncio
-SQL = SQL()
 
-class Cache():
+
+class Cache:
     def __init__(self):
-        self.cache={
-            "maps": {},
-            "settings": {}
-        }
+        self.cache = {"maps": {}, "settings": {}}
         asyncio.run(function(self._on_start_maps()))
         asyncio.run(function(self._on_start_settings()))
 
     # // CACHE MAPS INTO THE 'self.cache' DICTIONARY
     async def _on_start_maps(self):
-        rows = await SQL.select_all(f"SELECT * FROM maps")
+        rows = await SQL_CLASS().select_all(f"SELECT * FROM maps")
         for row in rows:
             if not row[0] in self.cache["maps"]:
                 self.cache["maps"][row[0]] = []
@@ -22,11 +19,10 @@ class Cache():
 
                 for m in maps:
                     maps.append(str(m[0].lower() + m[1:]))
-    
 
     # // CACHE SETTINGS INTO THE 'self.cache' DICTIONARY
     async def _on_start_settings(self):
-        rows = await SQL.select_all(f"SELECT * FROM settings")
+        rows = await SQL_CLASS().select_all(f"SELECT * FROM settings")
         for row in rows:
             if not row[0] in self.cache["settings"]:
                 self.cache["settings"][row[0]] = {
@@ -39,9 +35,8 @@ class Cache():
                     "win_elo": row[7],
                     "loss_elo": row[8],
                     "match_logs": row[9],
-                    "id": row[10]
+                    "id": row[10],
                 }
-
 
     # // RETURN CACHE DICTIONARY
     async def _cache(self):
