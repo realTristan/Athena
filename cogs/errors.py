d514e4b7a8d90d734dfc01200a94ba82e40943f3
tristan <heytristaann@gmail.com>
2021-11-02 18:30:53 -0400

    c

--- cogs/errors.py
+++ cogs/errors.py
@@ -5,51 +5,98 @@ class Error_Handling(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # // SORT COMMAND DESCRIPTIONS
-    # ////////////////////////////////
-    async def _sort_commands(self):
-        cogs=[]; cmds={}
-        for command in self.client.commands:
-            cogs.append(command.cog)
 
-        for c in list(cmds.fromkeys(cogs)):
-            if c is not None:
-                for i in list(cmds.fromkeys(c.get_commands())):
-                    cmds[i.name] = i.description
-        return cmds
-
-    # // GET THE SIMILAR COMMANDS TO AN UNKNWON COMMAND
-    # ////////////////////////////////////////////////////
-    async def _similar_cmds(self, ctx):
-        correct_commands = await self._sort_commands()
-        similar_commands=""
-        correct_usages = ""
-        commands = {}
-
-        for cmd in self.client.commands:
-            for c in str(cmd):
-                if c in (ctx.message.content.split(" ")[0]):
-                    if str(cmd) not in commands:
-                        commands[str(cmd)] = 0
-                    commands[str(cmd)] += 1
-
-        for s in commands:
-            if (commands[s] * 0.95) >= (len(s)/2):
-                similar_commands+=s+"\n"
-                correct_usages+=f"{s}: {correct_commands[s]}\n"
-        
-        if len(similar_commands.split("\n")) <= 1:
-            return await ctx.send(embed=discord.Embed(description=f"{ctx.author.mention} we could not find the command you are looking for", color=15158588))
-        return [similar_commands, correct_usages]
-
-    # // RETURN THE ERROR EMBED
+    # // RUN THE COMMAND SORTER
     # //////////////////////////////
-    async def _error_embed(self, ctx):
-        values = await self._similar_cmds(ctx)
-        embed=discord.Embed(title=f"Command Error: [{ctx.message.content}]", description="**Similar Commands:**\n"+values[0]+"\n**Command Usages:**\n"+values[1], color=15158588)
+    async def _run_sorter(self, ctx, _user_command):
+        _sorted_commands = await self._command_sort(_user_command)
+        
+        correct_usages=""; _correct_usages=[]
+        similar_commands=""; _similar_commands=[]
+        
+        # // CHECK IF THERE'S ANY SIMILAR COMMANDS
+        if len(_sorted_commands) <= 1:
+            return await ctx.send(embed=discord.Embed(description=f"{ctx.author.mention} we could not find the command you are looking for", color=15158588))
+
+        # // SORT COMMANDS INTO A LIST
+        for command in self.client.commands:
+            if str(command) in _sorted_commands:
+                _similar_commands.append(f"{str(command)}\n")
+                _correct_usages.append(f"{str(command)}: {command.description}\n")
+        
+        # // ADD THE COMMANDS TO A STRING
+        for x in range(round(len(_similar_commands)/1)):
+            similar_commands+=_similar_commands[x]
+            correct_usages+=_correct_usages[x]
+
+        embed=embed=discord.Embed(title=f"Command Error: [={_user_command}]", description=f"**Similar Commands:**\n{similar_commands}\n**Command Usages:**\n{correct_usages}", color=15158588)
         embed.set_footer(text="Message \"tristan#2230\" for support")
         return await ctx.send(embed=embed)
 
+    # CHECK HOW MANY TIMES A LETTER APPEARS IN BOTH WORDS
+    # //////////////////////////////////////////////////////
+    async def _letter_count(self, _user_command):
+        try:
+            _letter_dict = {}
+            for index in range(len(self.client.commands)):
+                if str(list(self.client.commands)[index]) not in _letter_dict:
+                    _letter_dict[str(list(self.client.commands)[index])] = {}
+
+                for letter in str(list(self.client.commands)[index]):
+                    if letter in list(_user_command):
+                        if letter not in _letter_dict[str(list(self.client.commands)[index])]:
+                            _letter_dict[str(list(self.client.commands)[index])][letter] = 0
+                        _letter_dict[str(list(self.client.commands)[index])][letter] += 1
+        except Exception:
+            return _letter_dict
+        return _letter_dict
+
+    # // CHECK LETTER POSITIONING
+    # ////////////////////////////////
+    async def _letter_position(self, _user_command):
+        _letter_dict = await self._letter_count(_user_command)
+        for command in self.client.commands:
+            for index in range(len(_user_command)):
+                try:
+                    if _user_command[index] == str(command)[index]:
+                        if _user_command[index] not in _letter_dict[str(command)]:
+                            _letter_dict[str(command)][str(command)[index]] = 0
+                        _letter_dict[str(command)][str(command)[index]] += 1.5
+                except Exception:
+                    pass
+        return _letter_dict
+
+    # // GIVE EACH COMMAND THEIR RATING
+    # /////////////////////////////////////
+    async def _command_rate(self, _user_command):
+        try:
+            _letter_dict = await self._letter_position(_user_command)
+            _result = {}
+            for command in _letter_dict:
+                if not command in _result:
+                    _result[command] = 0
+
+                for letter in _letter_dict[command]:
+                    _result[command] += _letter_dict[command][letter]
+        except Exception:
+            return _result
+        return _result
+
+    # SORT THE COMMANDS
+    # /////////////////////
+    async def _command_sort(self, _user_command):
+        try:
+            _command_dict = await self._command_rate(_user_command)
+            _sorted_command_dict = {k: v for k, v in sorted(_command_dict.items(), key=lambda item: item[1], reverse=True)}
+            _result = []
+
+            for command in _sorted_command_dict:
+                if _sorted_command_dict[command] > (len(command)*0.85):
+                    _result.append(command)
+        except Exception:
+            return _result
+        return _result
+    
 
     # ON COMMAND ERROR HANDLING
     # ///////////////////////////////
@@ -58,10 +105,9 @@ class Error_Handling(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             return await ctx.send(embed=discord.Embed(descriptionb=f"{ctx.author.mention} you do not have enough permissions", color=15158588))
             
-        await self._error_embed(ctx)
+        await self._run_sorter(ctx, str(ctx.message.content.split(" ")[0]).strip("="))
         if not isinstance(error, commands.CommandNotFound):
             raise error
-
 
 def setup(client):
     client.add_cog(Error_Handling(client))
